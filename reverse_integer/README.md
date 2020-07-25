76ce8b0f6eea35428092558953b47bfa41726417
YuanPei Li <conbas2019@gmail.com>
2020-07-25 18:08:10 +0800

    resolve 7. Reverse Integer

---
+++ reverse_integer/README.md
@@ -0,0 +1,3 @@
+# 7. Reverse Integer
+
+* [Link](https://leetcode.com/problems/reverse-integer/)
---
+++ reverse_integer/src/lib.rs
@@ -0,0 +1,39 @@
+struct Solution {}
+
+impl Solution {
+    pub fn reverse(x: i32) -> i32 {
+        let positive_reversed = x
+            .abs()
+            .to_string()
+            .chars()
+            .rev()
+            .collect::<String>()
+            .parse()
+            .unwrap_or(0);
+        if x.is_positive() {
+            positive_reversed
+        } else {
+            -positive_reversed
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_1() {
+        assert_eq!(Solution::reverse(123), 321);
+    }
+
+    #[test]
+    fn test_example_2() {
+        assert_eq!(Solution::reverse(-123), -321);
+    }
+
+    #[test]
+    fn test_example_3() {
+        assert_eq!(Solution::reverse(120), 21);
+    }
+}
