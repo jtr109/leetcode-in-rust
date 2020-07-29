469b24c07904c87ed05e77e21aa07f66a7a3a55f
jtr109 <conbas2019@gmail.com>
2020-07-29 23:27:53 +0800

    scan all distinct numbers

--- three_sum/src/lib.rs
+++ three_sum/src/lib.rs
@@ -6,26 +6,32 @@ struct Solution {}
 
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut sorted = nums.clone();
-        sorted.sort();
         let mut num_map = HashMap::new();
-        for (i, n) in sorted.iter().enumerate() {
-            num_map.entry(n).or_insert(vec![]).push(i);
+        for n in nums.iter() {
+            *num_map.entry(n).or_insert(0) += 1;
         }
         let mut sums = HashSet::new();
 
-        for i in 0..sorted.len() {
-            for j in (i + 2..sorted.len()).rev() {
-                let n = sorted[i];
-                let m = sorted[j];
-                let expected = 0 - n - m;
-                if let Some(indices) = num_map.get(&expected) {
-                    if indices.iter().filter(|&&k| i < k && k < j).count() > 0 {
-                        sums.insert(vec![n, expected, m]);
-                    }
+        let keys: Vec<&i32> = num_map.keys().cloned().collect();
+        for &&n in keys.iter() {
+            for &&m in keys.iter() {
+                let expected = 0 - m - n;
+                let group = vec![n, m, expected];
+                let mut group_map = HashMap::new();
+                for k in group.iter() {
+                    *group_map.entry(k).or_insert(0) += 1;
+                }
+                if group_map
+                    .iter()
+                    .all(|(&k, &count)| *num_map.get(k).unwrap_or(&0) >= count)
+                {
+                    let mut g = group.clone();
+                    g.sort();
+                    sums.insert(g);
                 }
             }
         }
+
         sums.iter().cloned().collect()
     }
 }
@@ -49,6 +55,17 @@ mod tests {
     fn test_example2() {
         let given = vec![-2, 0, 1, 1, 2];
         let expected: Vec<Vec<i32>> = vec![vec![-2, 0, 2], vec![-2, 1, 1]];
+        let got = Solution::three_sum(given);
+        assert_eq!(
+            got.iter().cloned().collect::<HashSet<Vec<i32>>>(),
+            expected.iter().cloned().collect::<HashSet<Vec<i32>>>(),
+        );
+    }
+
+    #[test]
+    fn test_example3() {
+        let given = vec![0, 0, 0];
+        let expected: Vec<Vec<i32>> = vec![vec![0, 0, 0]];
         let got = Solution::three_sum(given);
         assert_eq!(
             got.iter().cloned().collect::<HashSet<Vec<i32>>>(),
