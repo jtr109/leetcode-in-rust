c5af3ea638b778e7919747f5cdbbf1dd9fb806bf
jtr109 <conbas2019@gmail.com>
2020-07-30 23:45:22 +0800

    feat: ignore documentation test

--- kth_largest/src/lib.rs
+++ kth_largest/src/lib.rs
@@ -1,15 +1,17 @@
 #![allow(dead_code)]
 
-//! It works but slower than:
-//! ```rust,ignore
-//! impl Solution {
-//!     pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
-//!         let mut nums = nums;
-//!         nums.sort_by(|a, b| b.cmp(a));
-//!         nums[k as usize - 1]
-//!     }
-//! }
-//! ```
+/*!
+ * It works but slower than:
+ * ```rust,ignore
+ * impl Solution {
+ *     pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+ *         let mut nums = nums;
+ *         nums.sort_by(|a, b| b.cmp(a));
+ *         nums[k as usize - 1]
+ *     }
+ * }
+ * ```
+*/
 
 struct Solution {}
 
