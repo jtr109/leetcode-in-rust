1b9fc9610278741474324a5219d4fb6008c42f85
jtr109 <conbas2019@gmail.com>
2020-08-11 17:01:14 +0800

    add the solution of two sum

--- two_sum/src/lib.rs
+++ two_sum/src/lib.rs
@@ -9,10 +9,38 @@
 struct Solution {}
 
 // ----------------------------------------------------------------------------
+#[derive(Eq, Ord, PartialOrd, PartialEq)]
+struct Number {
+    index: usize,
+    value: i32,
+}
+
+impl Number {
+    fn new(index: usize, value: i32) -> Self {
+        Number { index, value }
+    }
+}
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        vec![0, 1]
+        let mut sorted = nums
+            .iter()
+            .enumerate()
+            .map(|(index, &value)| Number::new(index, value))
+            .collect::<Vec<Number>>();
+        sorted.sort_by(|a, b| a.value.cmp(&b.value));
+        let (mut low, mut high) = (0, sorted.len() - 1);
+        while low < high {
+            let s = sorted[low].value + sorted[high].value;
+            if s == target {
+                return vec![sorted[low].index as i32, sorted[high].index as i32];
+            } else if s < target {
+                low += 1;
+            } else {
+                high -= 1;
+            }
+        }
+        panic!("no match");
     }
 }
 
