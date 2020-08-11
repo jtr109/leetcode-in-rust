9191e8840a0e9b38a2c9b998fd8dd6f54f3595d3
jtr109 <conbas2019@gmail.com>
2020-08-11 19:14:21 +0800

    resolve the problem

--- implement_queue_using_stacks/src/lib.rs
+++ implement_queue_using_stacks/src/lib.rs
@@ -1,7 +1,54 @@
+/*!
+ * # 232. Implement Queue using Stacks
+ *
+ * * [Problem link](https://leetcode.com/problems/implement-queue-using-stacks/)
+ */
+
+#![allow(dead_code)]
+
+struct MyQueue {
+    start: usize,
+    data: Vec<i32>,
+}
+
+impl MyQueue {
+    fn new() -> Self {
+        MyQueue {
+            start: 0,
+            data: vec![],
+        }
+    }
+
+    fn push(&mut self, x: i32) {
+        self.data.push(x);
+    }
+
+    fn pop(&mut self) -> i32 {
+        let v = self.data[self.start];
+        self.start += 1;
+        v
+    }
+
+    fn peek(&self) -> i32 {
+        self.data[self.start]
+    }
+
+    fn empty(&self) -> bool {
+        self.start == self.data.len()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_example() {
+        let mut queue = MyQueue::new();
+        queue.push(1);
+        queue.push(2);
+        assert_eq!(queue.peek(), 1);
+        assert_eq!(queue.pop(), 1);
+        assert!(!queue.empty());
     }
 }
