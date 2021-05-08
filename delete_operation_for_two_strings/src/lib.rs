6b14ecd9bb5f8f4428e344486ad54e16068bb81e
Ryan Li <conbas2019@gmail.com>
2021-05-08 11:24:57 +0800

    feat: resolved with cache

--- delete_operation_for_two_strings/src/lib.rs
+++ delete_operation_for_two_strings/src/lib.rs
@@ -9,19 +9,25 @@ pub struct Solution {}
 
 impl Solution {
     pub fn min_distance(word1: String, word2: String) -> i32 {
-        let common_length = Self::lcs(&word1, &word2, word1.len(), word2.len());
+        let mut dp = vec![vec![None; word2.len() + 1]; word1.len() + 1];
+        let common_length = Self::lcs(&word1, &word2, word1.len(), word2.len(), &mut dp);
         (word1.len() + word2.len() - 2 * common_length) as i32
     }
 
-    pub fn lcs(s1: &str, s2: &str, m: usize, n: usize) -> usize {
+    fn lcs(s1: &str, s2: &str, m: usize, n: usize, dp: &mut Vec<Vec<Option<usize>>>) -> usize {
         // comparing first m characters in s1 and first n characters in s2
-        if m == 0 || n == 0 {
+        if let Some(length) = dp[m][n] {
+            return length;
+        }
+        let length = if m == 0 || n == 0 {
             0
         } else if s1.chars().nth(m - 1) == s2.chars().nth(n - 1) {
-            1 + Self::lcs(s1, s2, m - 1, n - 1)
+            1 + Self::lcs(s1, s2, m - 1, n - 1, dp)
         } else {
-            Self::lcs(s1, s2, m - 1, n).max(Self::lcs(s1, s2, m, n - 1))
-        }
+            Self::lcs(s1, s2, m - 1, n, dp).max(Self::lcs(s1, s2, m, n - 1, dp))
+        };
+        dp[m][n] = Some(length);
+        length
     }
 }
 
@@ -34,8 +40,9 @@ mod test {
         let word1 = "sea";
         let word2 = "eat";
         let expected = 2;
+        let mut dp = vec![vec![None; word2.len() + 1]; word1.len() + 1];
         assert_eq!(
-            Solution::lcs(word1, word2, word1.len(), word2.len()),
+            Solution::lcs(word1, word2, word1.len(), word2.len(), &mut dp),
             expected
         );
     }
