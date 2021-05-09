8ad9268846209dc9211bbd87473b0dbd13929686
Ryan Li <conbas2019@gmail.com>
2021-05-09 22:22:47 +0800

    feat: resolved but timeout

--- construct_target_array_with_multiple_sums/src/lib.rs
+++ construct_target_array_with_multiple_sums/src/lib.rs
@@ -6,12 +6,16 @@ pub struct Solution {}
 
 impl Solution {
     pub fn is_possible(target: Vec<i32>) -> bool {
+        if target.len() == 1 {
+            return target[0] == 1;
+        }
         let mut target = target;
         loop {
             let mut any_num_less_than_one = false;
             let mut all_nums_are_one = true;
-            let mut max_num = 0;
             let mut max_index = 0;
+            let mut second_max_index = 0;
+            // TODO: second_max_index 算法有问题
             let mut sum = 0;
             for (i, &n) in target.iter().enumerate() {
                 if !any_num_less_than_one && n < 1 {
@@ -21,9 +25,11 @@ impl Solution {
                     all_nums_are_one = false;
                 }
                 sum += n;
-                if max_num < n {
+                if n > target[max_index] {
+                    second_max_index = max_index;
                     max_index = i;
-                    max_num = n;
+                } else if n > target[second_max_index] {
+                    second_max_index = i;
                 }
             }
             if any_num_less_than_one {
@@ -32,7 +38,16 @@ impl Solution {
             if all_nums_are_one {
                 return true;
             }
-            target[max_index] = 2 * target[max_index] - sum;
+            loop {
+                // 如果 max_index 中新的数字还是大于第二大的数字，那么不需要循环，直接再次减去其他数字。
+                // 取余数
+                let others_sum = sum - target[max_index];
+                target[max_index] -= others_sum;
+                sum -= others_sum;
+                if target[max_index] <= target[second_max_index] {
+                    break;
+                }
+            }
         }
     }
 }
@@ -56,6 +71,24 @@ mod test {
     #[test]
     fn example_3() {
         let target = vec![8, 5];
+        assert!(Solution::is_possible(target));
+    }
+
+    #[test]
+    fn submission_1() {
+        let target = vec![5, 2];
+        assert!(Solution::is_possible(target));
+    }
+
+    #[test]
+    fn submission_2() {
+        let target = vec![9, 9, 9];
+        assert!(!Solution::is_possible(target));
+    }
+
+    #[test]
+    fn submission_3() {
+        let target = vec![1, 1, 61, 9, 17];
         assert!(Solution::is_possible(target));
     }
 }
