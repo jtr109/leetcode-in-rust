8f941f4b8d825095fe7cd00f930a71ee5c5ab0c4
Ryan Li <conbas2019@gmail.com>
2021-05-10 21:41:30 +0800

    feat: basic prime count

--- count_primes/src/lib.rs
+++ count_primes/src/lib.rs
@@ -2,14 +2,30 @@ pub struct Solution {}
 
 impl Solution {
     pub fn count_primes(n: i32) -> i32 {
-        1
+        (0..n).filter(|x| Self::is_prime(x)).count() as i32
+    }
+
+    pub fn is_prime(x: &i32) -> bool {
+        (2..*x).any(|i| *x % i == 0)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        assert_eq!(Solution::count_primes(10), 4);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(Solution::count_primes(0), 0);
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(Solution::count_primes(1), 0);
     }
 }
