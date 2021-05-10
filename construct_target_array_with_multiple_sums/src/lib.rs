2cfb762c4951c4e4a741b56c31ce903b9ed890f6
Ryan Li <conbas2019@gmail.com>
2021-05-10 10:10:07 +0800

    feat: use binary heap but also timed out

--- construct_target_array_with_multiple_sums/src/lib.rs
+++ construct_target_array_with_multiple_sums/src/lib.rs
@@ -2,52 +2,29 @@
  * https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3737/
  */
 
+use std::collections::BinaryHeap;
+
 pub struct Solution {}
 
-impl Solution {
+impl<'a> Solution {
     pub fn is_possible(target: Vec<i32>) -> bool {
-        if target.len() == 1 {
-            return target[0] == 1;
+        let mut heap = BinaryHeap::new();
+        let mut sum = 0;
+        for e in target.iter() {
+            heap.push(*e);
+            sum += e;
         }
-        let mut target = target;
         loop {
-            let mut any_num_less_than_one = false;
-            let mut all_nums_are_one = true;
-            let mut max_index = 0;
-            let mut second_max_index = 0;
-            // TODO: second_max_index 算法有问题
-            let mut sum = 0;
-            for (i, &n) in target.iter().enumerate() {
-                if !any_num_less_than_one && n < 1 {
-                    any_num_less_than_one = true;
-                }
-                if all_nums_are_one && n != 1 {
-                    all_nums_are_one = false;
-                }
-                sum += n;
-                if n > target[max_index] {
-                    second_max_index = max_index;
-                    max_index = i;
-                } else if n > target[second_max_index] {
-                    second_max_index = i;
-                }
-            }
-            if any_num_less_than_one {
-                return false;
-            }
-            if all_nums_are_one {
+            let max = heap.pop().unwrap();
+            if max == 1 {
                 return true;
             }
-            loop {
-                // 如果 max_index 中新的数字还是大于第二大的数字，那么不需要循环，直接再次减去其他数字。
-                // 取余数
-                let others_sum = sum - target[max_index];
-                target[max_index] -= others_sum;
-                sum -= others_sum;
-                if target[max_index] <= target[second_max_index] {
-                    break;
-                }
+            let stuff = 2 * max - sum;
+            if stuff < 1 {
+                return false;
             }
+            sum -= max - stuff;
+            heap.push(stuff);
         }
     }
 }
