c2a26e2b1b05f530b5028445bf858e1f82814707
Ryan Li <conbas2019@gmail.com>
2021-05-13 20:59:13 +0800

    feat: add template

--- ambiguous_coordinates/src/lib.rs
+++ ambiguous_coordinates/src/lib.rs
@@ -1,3 +1,15 @@
+/*!
+ * https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3741/
+ */
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn ambiguous_coordinates(s: String) -> Vec<String> {
+        vec![]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
