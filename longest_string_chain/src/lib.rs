447fa9d4193cde1caab4c0c4ceb3a07c2e7e3d4e
Ryan Li <conbas2019@gmail.com>
2021-05-17 15:22:07 +0800

    feat: add tests

--- longest_string_chain/src/lib.rs
+++ longest_string_chain/src/lib.rs
@@ -1,7 +1,30 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn longest_str_chain(words: Vec<String>) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let words = vec!["a", "b", "ba", "bca", "bda", "bdca"];
+        let expected = 4;
+        assert_eq!(
+            Solution::longest_str_chain(words.iter().map(|x| x.to_string()).collect()),
+            expected
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        let words = vec!["xbc", "pcxbcf", "xb", "cxbc", "pcxbc"];
+        let expected = 5;
+        assert_eq!(
+            Solution::longest_str_chain(words.iter().map(|x| x.to_string()).collect()),
+            expected
+        );
     }
 }
