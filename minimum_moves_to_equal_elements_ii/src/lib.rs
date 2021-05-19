bda0a31122673b87d82d4bba5065f09dcb4937c5
Ryan Li <conbas2019@gmail.com>
2021-05-19 18:04:15 +0800

    feat: resolve tests

--- minimum_moves_to_equal_elements_ii/src/lib.rs
+++ minimum_moves_to_equal_elements_ii/src/lib.rs
@@ -1,7 +1,34 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn min_moves2(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        nums.sort();
+        let index = nums.len() / 2;
+        let target = nums[index];
+        let left_count = nums[..index].iter().count();
+        let left_sum: i32 = nums[..index].iter().sum();
+        let right_count = nums[index..].iter().count();
+        let right_sum: i32 = nums[index..].iter().sum();
+        target * left_count as i32 - left_sum + right_sum - target * right_count as i32
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let nums = vec![1, 2, 3];
+        let expected = 2;
+        assert_eq!(Solution::min_moves2(nums), expected);
+    }
+
+    #[test]
+    fn example_2() {
+        let nums = vec![1, 10, 2, 9];
+        let expected = 16;
+        assert_eq!(Solution::min_moves2(nums), expected);
     }
 }
