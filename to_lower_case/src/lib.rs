a304f418ee8d78547755e43198d0c0805177a4e3
Ryan Li <conbas2019@gmail.com>
2021-05-24 17:31:14 +0800

    feat: resolved

--- to_lower_case/src/lib.rs
+++ to_lower_case/src/lib.rs
@@ -1,7 +1,36 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn to_lower_case(s: String) -> String {
+        s.chars()
+            .map(|c| match c {
+                'A'..='Z' => (c as u8 - 'A' as u8 + 'a' as u8) as char,
+                _ => c,
+            })
+            .collect()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    fn ok(s: &str, expected: &str) {
+        assert_eq!(Solution::to_lower_case(s.to_string()), expected.to_string());
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        ok("Hello", "hello");
+    }
+
+    #[test]
+    fn example_2() {
+        ok("here", "here");
+    }
+
+    #[test]
+    fn example_3() {
+        ok("LOVELY", "lovely");
     }
 }
