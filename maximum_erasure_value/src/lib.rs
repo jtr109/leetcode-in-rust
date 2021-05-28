9e111d28f044c5eb6ede285dc99bb812e6c2fe92
Ryan Li <conbas2019@gmail.com>
2021-05-28 16:21:10 +0800

    feat: add tests

--- maximum_erasure_value/src/lib.rs
+++ maximum_erasure_value/src/lib.rs
@@ -1,7 +1,24 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn maximum_unique_subarray(nums: Vec<i32>) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let nums = vec![4, 2, 4, 5, 6];
+        let expected = 17;
+        assert_eq!(Solution::maximum_unique_subarray(nums), expected);
+    }
+
+    #[test]
+    fn example_2() {
+        let nums = vec![5, 2, 1, 2, 5, 2, 1, 2, 5];
+        let expected = 8;
+        assert_eq!(Solution::maximum_unique_subarray(nums), expected);
     }
 }
