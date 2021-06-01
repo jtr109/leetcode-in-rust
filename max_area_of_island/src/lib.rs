cdcc99f9f4ede415e78688d410019004c5c80a8b
Ryan Li <conbas2019@gmail.com>
2021-06-01 17:20:10 +0800

    feat: add tests

--- max_area_of_island/src/lib.rs
+++ max_area_of_island/src/lib.rs
@@ -1,7 +1,39 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn max_area_of_island(grid: Vec<Vec<i32>>) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let grid = [
+            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+        ];
+        let expected = 6;
+        assert_eq!(
+            Solution::max_area_of_island(grid.iter().map(|row| row.to_vec()).collect()),
+            expected
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        let grid = [[0, 0, 0, 0, 0, 0, 0, 0]];
+        let expected = 0;
+        assert_eq!(
+            Solution::max_area_of_island(grid.iter().map(|row| row.to_vec()).collect()),
+            expected
+        );
     }
 }
