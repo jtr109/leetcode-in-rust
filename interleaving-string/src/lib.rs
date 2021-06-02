9a59c2ee20c8edd0aa355eac789dfdecc2047bc3
Ryan Li <conbas2019@gmail.com>
2021-06-02 15:48:14 +0800

    feat: add submission test

--- interleaving-string/src/lib.rs
+++ interleaving-string/src/lib.rs
@@ -1,19 +1,20 @@
 pub struct Solution {}
 
 impl Solution {
-    fn interleave(s1: &str, s2: &str, s3: &str) -> bool {
-        if s3.chars().count() == 0 {
-            return s1.chars().count() == 0 && s2.chars().count() == 0;
+    fn interleave(s1: &[char], s2: &[char], s3: &[char]) -> bool {
+        if s3.len() != s1.len() + s2.len() {
+            return false;
+        }
+        if s3.len() == 0 {
+            return s1.len() == 0 && s2.len() == 0;
         }
         if s1.len() == 0 {
             s2 == s3
         } else if s2.len() == 0 {
             s1 == s3
-        } else if s1.chars().next() == s3.chars().next() && Self::interleave(&s1[1..], s2, &s3[1..])
-        {
+        } else if s1[0] == s3[0] && Self::interleave(&s1[1..].to_vec(), s2, &s3[1..].to_vec()) {
             true
-        } else if s2.chars().next() == s3.chars().next() && Self::interleave(s1, &s2[1..], &s3[1..])
-        {
+        } else if s2[0] == s3[0] && Self::interleave(s1, &s2[1..], &s3[1..]) {
             true
         } else {
             false
@@ -21,7 +22,11 @@ impl Solution {
     }
 
     pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {
-        Self::interleave(&s1, &s2, &s3)
+        Self::interleave(
+            &s1.chars().collect::<Vec<char>>()[..],
+            &s2.chars().collect::<Vec<char>>()[..],
+            &s3.chars().collect::<Vec<char>>()[..],
+        )
     }
 }
 
@@ -59,6 +64,15 @@ mod tests {
         let s1 = "";
         let s2 = "";
         let s3 = "";
+        let expected = true;
+        as_expected(s1, s2, s3, expected);
+    }
+
+    #[test]
+    fn submission_1() {
+        let s1 = "bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa";
+        let s2 = "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab";
+        let s3 = "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab";
         let expected = true;
         as_expected(s1, s2, s3, expected);
     }
