8548208bb73bf2f3d19ab1044ea0b8c16c0a22e3
Ryan Li <conbas2019@gmail.com>
2021-06-02 15:34:33 +0800

    feat: resolved

--- interleaving-string/src/lib.rs
+++ interleaving-string/src/lib.rs
@@ -1,7 +1,27 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {}
+    fn interleave(s1: &str, s2: &str, s3: &str) -> bool {
+        if s3.len() == 0 {
+            true
+        } else if s1.len() == 0 {
+            s2 == s3
+        } else if s2.len() == 0 {
+            s1 == s3
+        } else if s1.chars().next() == s3.chars().next() && Self::interleave(&s1[1..], s2, &s3[1..])
+        {
+            true
+        } else if s2.chars().next() == s3.chars().next() && Self::interleave(s1, &s2[1..], &s3[1..])
+        {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {
+        Self::interleave(&s1, &s2, &s3)
+    }
 }
 
 #[cfg(test)]
