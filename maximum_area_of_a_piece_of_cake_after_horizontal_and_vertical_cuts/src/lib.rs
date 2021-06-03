3234b7302e7f9d1f31c24b183658e9b590fad396
Ryan Li <conbas2019@gmail.com>
2021-06-03 15:34:48 +0800

    feat: resolved but not all

--- maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/src/lib.rs
+++ maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/src/lib.rs
@@ -1,7 +1,25 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn max_area(h: i32, w: i32, horizontal_cuts: Vec<i32>, vertical_cuts: Vec<i32>) -> i32 {}
+    pub fn max_area(h: i32, w: i32, horizontal_cuts: Vec<i32>, vertical_cuts: Vec<i32>) -> i32 {
+        let mut horizontal_lines = vec![vec![0, h], horizontal_cuts].concat();
+        let mut vertical_lines = vec![vec![0, w], vertical_cuts].concat();
+        horizontal_lines.sort();
+        vertical_lines.sort();
+        horizontal_lines[..horizontal_lines.len() - 1]
+            .iter()
+            .zip(horizontal_lines[1..].iter())
+            .map(|(u, d)| d - u)
+            .max()
+            .unwrap()
+            * vertical_lines[..vertical_lines.len() - 1]
+                .iter()
+                .zip(vertical_lines[1..].iter())
+                .map(|(l, r)| r - l)
+                .max()
+                .unwrap()
+            % ((10 as i32).pow(9) + 7)
+    }
 }
 
 #[cfg(test)]
@@ -38,7 +56,7 @@ mod tests {
     fn example_3() {
         let h = 5;
         let w = 4;
-        let horizontal_cuts = [3, 1];
+        let horizontal_cuts = [3];
         let vertical_cuts = [3];
         let expected = 9;
         assert_eq!(
