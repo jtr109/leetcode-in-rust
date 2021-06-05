c8ecc16e2c14ba77d658e31054ff8b843cf6b12f
Ryan Li <conbas2019@gmail.com>
2021-06-05 20:47:06 +0800

    feat: add tests

--- maximum_performance_of_a_team/src/lib.rs
+++ maximum_performance_of_a_team/src/lib.rs
@@ -1,7 +1,49 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn max_performance(n: i32, speed: Vec<i32>, efficiency: Vec<i32>, k: i32) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let n = 6;
+        let speed = [2, 10, 3, 1, 5, 8];
+        let efficiency = [5, 4, 3, 9, 7, 2];
+        let k = 2;
+        let expected = 60;
+        assert_eq!(
+            Solution::max_performance(n, speed.to_vec(), efficiency.to_vec(), k),
+            expected
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        let n = 6;
+        let speed = [2, 10, 3, 1, 5, 8];
+        let efficiency = [5, 4, 3, 9, 7, 2];
+        let k = 3;
+        let expected = 68;
+        assert_eq!(
+            Solution::max_performance(n, speed.to_vec(), efficiency.to_vec(), k),
+            expected
+        );
+    }
+
+    #[test]
+    fn example_3() {
+        let n = 6;
+        let speed = [2, 10, 3, 1, 5, 8];
+        let efficiency = [5, 4, 3, 9, 7, 2];
+        let k = 4;
+        let expected = 72;
+        assert_eq!(
+            Solution::max_performance(n, speed.to_vec(), efficiency.to_vec(), k),
+            expected
+        );
     }
 }
