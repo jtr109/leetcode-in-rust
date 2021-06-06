b86556b6e4869464737eaa4a540c762e66601be8
Ryan Li <conbas2019@gmail.com>
2021-06-06 18:42:04 +0800

    feat: add tests

--- longest_consecutive_sequence/src/lib.rs
+++ longest_consecutive_sequence/src/lib.rs
@@ -1,7 +1,23 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let nums = [100, 4, 200, 1, 3, 2];
+        let expected = 4;
+        assert_eq!(Solution::longest_consecutive(nums.to_vec()), expected);
+    }
+
+    fn example_2() {
+        let nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
+        let expected = 9;
+        assert_eq!(Solution::longest_consecutive(nums.to_vec()), expected);
     }
 }
