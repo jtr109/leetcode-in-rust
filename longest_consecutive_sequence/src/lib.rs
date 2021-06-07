4c6f3b28971959b7bf4a0759bbcda3410b166e5f
Ryan Li <conbas2019@gmail.com>
2021-06-07 20:15:15 +0800

    feat: module

--- longest_consecutive_sequence/src/lib.rs
+++ longest_consecutive_sequence/src/lib.rs
@@ -1,7 +1,30 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {}
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        if nums.len() == 0 {
+            return 0;
+        }
+
+        let mut numbers = nums.clone();
+        numbers.sort();
+        numbers.dedup();
+
+        let mut max_length = 0;
+        let mut temp_length = 1;
+        for n in numbers.windows(2) {
+            let j = n[1];
+            let i = n[0];
+            if j == i + 1 {
+                temp_length += 1;
+                continue;
+            }
+            max_length = max_length.max(temp_length);
+            temp_length = 1;
+        }
+        max_length = max_length.max(temp_length);
+        max_length
+    }
 }
 
 #[cfg(test)]
@@ -15,6 +38,7 @@ mod tests {
         assert_eq!(Solution::longest_consecutive(nums.to_vec()), expected);
     }
 
+    #[test]
     fn example_2() {
         let nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
         let expected = 9;
