c1d275b0ccf546860774b744275b0d3a5812894d
Ryan Li <conbas2019@gmail.com>
2021-06-07 20:17:17 +0800

    feat: add tests

--- min_cost_climbing_stairs/src/lib.rs
+++ min_cost_climbing_stairs/src/lib.rs
@@ -1,7 +1,24 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let cost = [10, 15, 20];
+        let expected = 15;
+        assert_eq!(Solution::min_cost_climbing_stairs(cost.to_vec()), expected);
+    }
+
+    #[test]
+    fn example_2() {
+        let cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
+        let expected = 6;
+        assert_eq!(Solution::min_cost_climbing_stairs(cost.to_vec()), expected);
     }
 }
