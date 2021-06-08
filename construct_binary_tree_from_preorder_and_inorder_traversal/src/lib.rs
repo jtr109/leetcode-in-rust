d2f9c34fdfb4d7d7825744ebff2ebe4204a81557
Ryan Li <conbas2019@gmail.com>
2021-06-08 18:44:32 +0800

    feat: add tests

--- construct_binary_tree_from_preorder_and_inorder_traversal/src/lib.rs
+++ construct_binary_tree_from_preorder_and_inorder_traversal/src/lib.rs
@@ -1,7 +1,74 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let preorder = [3, 9, 20, 15, 7];
+        let inorder = [9, 3, 15, 20, 7];
+        let expected = Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 9,
+                left: None,
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 20,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 15,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 7,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+        })));
+        assert_eq!(
+            Solution::build_tree(preorder.to_vec(), inorder.to_vec()),
+            expected
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        let preorder = [-1];
+        let inorder = [-1];
+        let expected = Some(Rc::new(RefCell::new(TreeNode::new(-1))));
+        assert_eq!(
+            Solution::build_tree(preorder.to_vec(), inorder.to_vec()),
+            expected
+        );
     }
 }
