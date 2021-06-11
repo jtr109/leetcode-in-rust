4920d8bd90b7e1a6548c6134db6476a3e57763b5
Ryan Li <conbas2019@gmail.com>
2021-06-11 15:50:28 +0800

    feat: use anchor

--- stone_game_vii/src/lib.rs
+++ stone_game_vii/src/lib.rs
@@ -6,19 +6,19 @@ enum Turn {
 }
 
 impl Solution {
-    fn operate(stones: &[i32], turn: &Turn) -> i32 {
-        if stones.len() == 0 {
+    fn operate(stones: &Vec<i32>, i: usize, j: usize, turn: &Turn) -> i32 {
+        if i + 1 == j {
             return 0;
         }
         let next_turn = match turn {
             Turn::AliceTurn => Turn::BobTurn,
             Turn::BobTurn => Turn::AliceTurn,
         };
-        let stones_without_leftmost = &stones[1..];
-        let difference_removing_leftmost = Self::operate(stones_without_leftmost, &next_turn); // 如果移动最左边的石头，后续产生的差异
+        let stones_without_leftmost = &stones[i + 1..j];
+        let difference_removing_leftmost = Self::operate(stones, i + 1, j, &next_turn); // 如果移动最左边的石头，后续产生的差异
         let score_removing_leftmost = stones_without_leftmost.iter().sum::<i32>();
-        let stones_without_rightmost = &stones[..stones.len() - 1];
-        let difference_removing_rightmost = Self::operate(stones_without_rightmost, &next_turn);
+        let stones_without_rightmost = &stones[i..j - 1];
+        let difference_removing_rightmost = Self::operate(stones, i, j - 1, &next_turn);
         let score_removing_rightmost = stones_without_rightmost.iter().sum::<i32>();
         match turn {
             Turn::AliceTurn => (difference_removing_leftmost + score_removing_leftmost)
@@ -29,7 +29,7 @@ impl Solution {
     }
 
     pub fn stone_game_vii(stones: Vec<i32>) -> i32 {
-        Self::operate(&stones, &Turn::AliceTurn)
+        Self::operate(&stones, 0, stones.len(), &Turn::AliceTurn)
     }
 }
 
