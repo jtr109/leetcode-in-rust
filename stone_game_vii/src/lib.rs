27a1268184c123d8d9055b556dafb69f56deeef8
Ryan Li <conbas2019@gmail.com>
2021-06-11 15:13:05 +0800

    feat: add tests

--- stone_game_vii/src/lib.rs
+++ stone_game_vii/src/lib.rs
@@ -1,7 +1,24 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn stone_game_vii(stones: Vec<i32>) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let stones = [5, 3, 1, 4, 2];
+        let expected = 6;
+        assert_eq!(Solution::stone_game_vii(stones.to_vec()), expected);
+    }
+
+    #[test]
+    fn example_2() {
+        let stones = [7, 90, 5, 1, 100, 10, 10, 2];
+        let expected = 122;
+        assert_eq!(Solution::stone_game_vii(stones.to_vec()), expected);
     }
 }
