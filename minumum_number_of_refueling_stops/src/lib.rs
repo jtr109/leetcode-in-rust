9fffb11effb486ee84f57530b73b8c4ac467ed1d
Ryan Li <conbas2019@gmail.com>
2021-06-12 20:22:54 +0800

    feat: resolved but not understand

--- minumum_number_of_refueling_stops/src/lib.rs
+++ minumum_number_of_refueling_stops/src/lib.rs
@@ -1,7 +1,23 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn min_refuel_stops(target: i32, start_fuel: i32, stations: Vec<Vec<i32>>) -> i32 {}
+    pub fn min_refuel_stops(target: i32, start_fuel: i32, stations: Vec<Vec<i32>>) -> i32 {
+        let mut furthest = vec![0; stations.len() + 1]; // furthest distance after ith refueling
+        furthest[0] = start_fuel;
+        for i in 0..stations.len() {
+            for t in 0..i + 1 {
+                if furthest[t] >= stations[i][0] {
+                    furthest[t + 1] = furthest[t + 1].max(furthest[t] + stations[i][1]);
+                }
+            }
+        }
+        for (t, d) in furthest.iter().enumerate() {
+            if *d >= target {
+                return t as i32;
+            }
+        }
+        -1
+    }
 }
 
 #[cfg(test)]
@@ -21,7 +37,7 @@ mod tests {
                 stations
                     .to_vec()
                     .iter()
-                    .map(|x| x.to_vec())
+                    .map(|x: &[i32; 2]| x.to_vec())
                     .collect::<Vec<Vec<i32>>>()
             ),
             expected
@@ -53,7 +69,7 @@ mod tests {
         let target = 100;
         let start_fuel = 10;
         let stations = [[10, 60], [20, 30], [30, 30], [60, 40]];
-        let expected = 0;
+        let expected = 2;
         assert_eq!(
             Solution::min_refuel_stops(
                 target,
