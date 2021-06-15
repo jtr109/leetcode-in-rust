ffbd09bbfbf28fdd17b563fffbf6d5ab6cecf616
Ryan Li <conbas2019@gmail.com>
2021-06-15 22:21:50 +0800

    feat: resovled

--- matchsticks_to_square/src/lib.rs
+++ matchsticks_to_square/src/lib.rs
@@ -1,7 +1,34 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn makesquare(matchsticks: Vec<i32>) -> bool {}
+    pub fn makesquare(matchsticks: Vec<i32>) -> bool {
+        let sum: i32 = matchsticks.iter().sum();
+        if sum % 4 != 0 {
+            return false;
+        }
+        let target = sum / 4;
+        let sums = vec![0; 4];
+        let nums = matchsticks.iter().cloned().rev().collect();
+        Self::dfs(&nums, &sums, 0, target)
+    }
+
+    fn dfs(nums: &Vec<i32>, sums: &Vec<i32>, index: usize, target: i32) -> bool {
+        if index == nums.len() {
+            return sums.iter().all(|x| *x == target);
+        }
+        let n = nums[index];
+        for (i, s) in sums.iter().enumerate() {
+            if s + n > target {
+                continue;
+            }
+            let mut new_sums = sums.clone();
+            new_sums[i] = s + n;
+            if Self::dfs(nums, &new_sums, index + 1, target) {
+                return true;
+            }
+        }
+        false
+    }
 }
 
 #[cfg(test)]
