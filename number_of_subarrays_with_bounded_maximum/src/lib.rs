8559963e7c0c6f7868bc03b91c1e7acad40db3a3
Ryan Li <conbas2019@gmail.com>
2021-06-17 15:56:21 +0800

    feat: wrong answer when submitted

--- number_of_subarrays_with_bounded_maximum/src/lib.rs
+++ number_of_subarrays_with_bounded_maximum/src/lib.rs
@@ -1,7 +1,24 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn num_subarray_bounded_max(nums: Vec<i32>, left: i32, right: i32) -> i32 {}
+    pub fn num_subarray_bounded_max(nums: Vec<i32>, left: i32, right: i32) -> i32 {
+        let mut count = 0;
+        let mut at_most_left = 0; // 从左往右最后一个大于等于 left 的元素对应的索引
+        let mut subarray_left = 0;
+        for i in 0..nums.len() {
+            if nums[i] > right {
+                count += at_most_left - subarray_left;
+                at_most_left = i + 1;
+                subarray_left = i + 1;
+                continue;
+            }
+            count += 1;
+            if nums[i] >= left {
+                at_most_left = i;
+            }
+        }
+        count as i32
+    }
 }
 
 #[cfg(test)]
@@ -14,6 +31,18 @@ mod tests {
         let left = 2;
         let right = 3;
         let expected = 3;
+        assert_eq!(
+            Solution::num_subarray_bounded_max(nums.to_vec(), left, right),
+            expected
+        );
+    }
+
+    #[test]
+    fn submission_1() {
+        let nums = [2, 9, 2, 5, 6];
+        let left = 2;
+        let right = 8;
+        let expected = 7;
         assert_eq!(
             Solution::num_subarray_bounded_max(nums.to_vec(), left, right),
             expected
