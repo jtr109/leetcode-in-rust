df176864194bbd862b441b322cdeb3fbe9011172
Ryan Li <conbas2019@gmail.com>
2021-06-17 15:34:02 +0800

    feat: add a test

--- number_of_subarrays_with_bounded_maximum/src/lib.rs
+++ number_of_subarrays_with_bounded_maximum/src/lib.rs
@@ -1,7 +1,22 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn num_subarray_bounded_max(nums: Vec<i32>, left: i32, right: i32) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let nums = [2, 1, 4, 3];
+        let left = 2;
+        let right = 3;
+        let expected = 3;
+        assert_eq!(
+            Solution::num_subarray_bounded_max(nums.to_vec(), left, right),
+            expected
+        );
     }
 }
