2615aa2683354cba4df6706f5ddea122818aa320
Ryan Li <conbas2019@gmail.com>
2021-06-18 15:29:47 +0800

    feat: resolved but low performance

--- range_sum_query_mutable/src/lib.rs
+++ range_sum_query_mutable/src/lib.rs
@@ -1,15 +1,33 @@
-struct NumArray {}
+/*!
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
+## 解决方案 1
+
+直接实现一个结构体，包含 vector，在 vector 中管理 nums。
+
+这样的方案存在的问题是每次都需要从各个索引重新计算 sum_range，会带来性能损失。
+
+## 方案 2
+
+缓存前 i 个数的和，这样的好处是 `sum_range` 的时候只需要将记录的和相减即可。
+
  */
+
+pub struct NumArray {
+    nums: Vec<i32>,
+}
+
 impl NumArray {
-    fn new(nums: Vec<i32>) -> Self {}
+    pub fn new(nums: Vec<i32>) -> Self {
+        Self { nums }
+    }
 
-    fn update(&self, index: i32, val: i32) {}
+    pub fn update(&mut self, index: i32, val: i32) {
+        self.nums[index as usize] = val;
+    }
 
-    fn sum_range(&self, left: i32, right: i32) -> i32 {}
+    pub fn sum_range(&self, left: i32, right: i32) -> i32 {
+        self.nums[left as usize..=right as usize].iter().sum()
+    }
 }
 
 #[cfg(test)]
@@ -19,7 +37,7 @@ mod tests {
     #[test]
     fn example_1() {
         let nums = [1, 3, 5];
-        let obj = NumArray::new(nums.to_vec());
+        let mut obj = NumArray::new(nums.to_vec());
         assert_eq!(obj.sum_range(0, 2), 9);
         obj.update(1, 2);
         assert_eq!(obj.sum_range(0, 2), 8);
