b92229b5515a4d5fba020b9ab5a4f8895bc67076
Ryan Li <conbas2019@gmail.com>
2021-06-18 15:18:20 +0800

    feat: add tests

--- range_sum_query_mutable/src/lib.rs
+++ range_sum_query_mutable/src/lib.rs
@@ -1,7 +1,27 @@
+struct NumArray {}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl NumArray {
+    fn new(nums: Vec<i32>) -> Self {}
+
+    fn update(&self, index: i32, val: i32) {}
+
+    fn sum_range(&self, left: i32, right: i32) -> i32 {}
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn example_1() {
+        let nums = [1, 3, 5];
+        let obj = NumArray::new(nums.to_vec());
+        assert_eq!(obj.sum_range(0, 2), 9);
+        obj.update(1, 2);
+        assert_eq!(obj.sum_range(0, 2), 8);
     }
 }
